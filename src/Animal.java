152326451b4128efe8a6672157f68334ecae7daf
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2024-04-11 21:40:09 +0300

    homework-2

---
+++ src/Animal.java
@@ -0,0 +1,37 @@
+public class Animal {
+    private static int count;
+    protected String name;
+    protected int runLimit;
+    protected int swimLimit;
+
+    public Animal(String name, int runLimit, int swimLimit) {
+        this.name = name;
+        this.runLimit = runLimit;
+        this.swimLimit = swimLimit;
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public void run(int distance) {
+        if (runLimit == 0) {
+            System.out.println(name + " cannot run");
+        } else if (distance <= runLimit) {
+            System.out.println(name + " has run " + distance + " m");
+        } else {
+            System.out.println(name + " cannot run " + distance + " m");
+        }
+    }
+
+    public void swim(int distance) {
+        if (swimLimit == 0) {
+            System.out.println(name + " cannot swim");
+        } else if (distance <= swimLimit) {
+            System.out.println(name + " has swam " + distance + " m");
+        } else {
+            System.out.println(name + " cannot swim " + distance + " m");
+        }
+    }
+}
