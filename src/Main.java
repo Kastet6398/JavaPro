d71133d4cd1bb57613dd1f14b4e4d59a7fa0ce20
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2024-04-12 08:13:58 +0300

    Revert "hw2 solution1"
    This reverts commit 39b3d24dcf63821456edd91bc59e072cc74bb75b.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Dog dogBobik = new Dog("Bobik");
-        Cat catMurka = new Cat("Murka");
-
-        dogBobik.run(150);
-        dogBobik.run(600);
-        dogBobik.swim(5);
-        dogBobik.swim(15);
-
-        catMurka.run(100);
-        catMurka.run(250);
-        catMurka.swim(10);
-
-        System.out.println("Dog count: " + Dog.getCount());
-        System.out.println("Cat count: " + Cat.getCount());
-        System.out.println("Animal count: " + Animal.getCount());
+        System.out.println("Hello world!");
     }
 }
