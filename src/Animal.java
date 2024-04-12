ffdf77fc838a9fa6e73ff5cd83b5dccfd8dc7714
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2024-04-12 07:47:16 +0300

    cannot run a negative distance

--- src/Animal.java
+++ src/Animal.java
@@ -1,8 +1,8 @@
 public class Animal {
     private static int count;
-    protected String name;
-    protected int runLimit;
-    protected int swimLimit;
+    private final String name;
+    private final int runLimit;
+    private final int swimLimit;
 
     public Animal(String name, int runLimit, int swimLimit) {
         this.name = name;
@@ -18,6 +18,8 @@ public class Animal {
     public void run(int distance) {
         if (runLimit == 0) {
             System.out.println(name + " cannot run");
+        } else if (distance <= 0) {
+            System.out.println(name + " cannot run a negative distance");
         } else if (distance <= runLimit) {
             System.out.println(name + " has run " + distance + " m");
         } else {
@@ -28,7 +30,9 @@ public class Animal {
     public void swim(int distance) {
         if (swimLimit == 0) {
             System.out.println(name + " cannot swim");
-        } else if (distance <= swimLimit) {
+        } else if (distance <= 0) {
+            System.out.println(name + " cannot run a negative distance");
+        }  else if (distance <= swimLimit) {
             System.out.println(name + " has swam " + distance + " m");
         } else {
             System.out.println(name + " cannot swim " + distance + " m");
