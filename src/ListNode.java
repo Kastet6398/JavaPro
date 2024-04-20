bbb809f5afcf416f8752c0fe701b4e99b9099b92
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2024-04-20 16:06:09 +0300

    hw6

---
+++ src/ListNode.java
@@ -0,0 +1,36 @@
+public class ListNode {
+    private int val = 0;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    private ListNode next;
+
+    public ListNode() {
+    }
+    public ListNode(int val) {
+        this.val = val;
+    }
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ListNode ln5 = new ListNode(5);
+        ListNode ln4 = new ListNode(4, ln5);
+        ListNode ln3 = new ListNode(3, ln4);
+        ListNode ln2 = new ListNode(2, ln3);
+        ListNode head = new ListNode(1, ln2);
+
+        Solution.reverseList(head);
+
+        ListNode c = ln5;
+        while (c != null) {
+            System.out.println(c);
+            c=c.getNext();
+        }
     }
 }
