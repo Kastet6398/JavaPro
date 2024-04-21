d585d8fda5266fe12ad78449f08c31e82a259280
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2024-04-21 16:52:29 +0300

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode ln2 = new ListNode(2);
+        ListNode ln3 = new ListNode(3);
+        ListNode ln4 = new ListNode(4);
         ListNode ln5 = new ListNode(5);
-        ListNode ln4 = new ListNode(4, ln5);
-        ListNode ln3 = new ListNode(3, ln4);
-        ListNode ln2 = new ListNode(2, ln3);
-        ListNode head = new ListNode(1, ln2);
+
+        head.setNext(ln2);
+        ln2.setNext(ln3);
+        ln3.setNext(ln4);
+        ln4.setNext(ln5);
 
         Solution.reverseList(head);
 
