5f6ba7ee69318c9bb8b0b693a48e22313a3f58d0
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2024-05-09 21:29:25 +0300

    hw9

---
+++ src/Task1.java
@@ -0,0 +1,24 @@
+public class Task1 {
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("\\W", "");
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("race a car"));
+        System.out.println(isPalindrome(" "));
+    }
+}
