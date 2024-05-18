7ae89319761f88c1f322f9e14d5b42654bd39693
Konstantin Nezhalsky <konstantin.nezhalsky@gmail.com>
2024-05-18 12:30:25 +0300

    fix

--- src/BookAnalyzer.java
+++ src/BookAnalyzer.java
@@ -20,15 +20,18 @@ public class BookAnalyzer {
         }
 
         // Read the book and analyze text
-	int numOfWords = 0;
-	List<Map.Entry<String, Long>> sortedWordList;
+        int numOfUniqueWords = 0;
+        long numOfUniqueWordsWithLengthMoreThanTwo = 0;
+        List<Map.Entry<String, Long>> topTenWords;
 
         try (Stream<String> lines = Files.lines(bookFile.toPath())) {
-	    List<String> lineList = lines.flatMap(line -> Arrays.stream(line.toLowerCase().replaceAll("[^a-z\\s]", "").split("\\s+"))).collect(Collectors.toList());
-	    numOfWords = new HashSet<String>(lineList).size();
-            sortedWordList = lineList
-		    .stream()
-		    .filter(word -> word.length() > 2)
+            List<String> wordList = lines.flatMap(line -> Arrays.stream(line.toLowerCase().replaceAll("[^\\p{L}\\s]", "").split("\\s+"))).collect(Collectors.toList());
+            Set<String> wordSet = new HashSet<>(wordList);
+	    numOfUniqueWordsWithLengthMoreThanTwo = wordSet.stream().filter(word -> word.length() > 2).count();
+	    numOfUniqueWords = wordSet.size();
+            topTenWords = wordList
+        	    .stream()
+        	    .filter(word -> word.length() > 2)
                     .collect(Collectors.collectingAndThen(
                         Collectors.groupingBy(
                             s -> s,
@@ -37,9 +40,9 @@ public class BookAnalyzer {
                         map -> {
                             List<Map.Entry<String, Long>> entryList = new ArrayList<>(map.entrySet());
                             entryList.sort(Map.Entry.<String, Long>comparingByValue().reversed());
-                            return entryList;
+                            return entryList.subList(0, Math.min(entryList.size(), 10));
                         }
-    		    ));
+            	    ));
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
             return;
@@ -47,16 +50,19 @@ public class BookAnalyzer {
 
         // Most popular words
         System.out.println("10 most popular words with length > 2:");
-        sortedWordList.stream()
-                .limit(10)
-                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+	for (Map.Entry<String, Long> word : topTenWords) {
+	    System.out.println(word.getKey() + " -> " + word.getValue());
+	}
 
         // Output the number of unique words
-        System.out.println("Number of unique words: " + numOfWords);
+        System.out.println("Number of unique words: " + numOfUniqueWords);
 
+        System.out.println("Number of unique words with length > 2: " + numOfUniqueWordsWithLengthMoreThanTwo);
         // Write the statistics into the stats file
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(bookName + "_statistics.txt"))) {
-            sortedWordList.forEach(entry -> {
+            writer.write("10 most popular words with length > 2:");
+            writer.newLine();
+	    topTenWords.forEach(entry -> {
                 try {
                     writer.write(entry.getKey() + " -> " + entry.getValue());
                     writer.newLine();
@@ -64,7 +70,9 @@ public class BookAnalyzer {
                     e.printStackTrace();
                 }
             });
-            writer.write("Number of unique words: " + numOfWords);
+            writer.write("Number of unique words: " + numOfUniqueWords);
+	    writer.newLine();
+            writer.write("Number of unique words with length > 2: " + numOfUniqueWordsWithLengthMoreThanTwo);
         } catch (IOException e) {
             System.out.println("Error writing the statistics to file: " + e.getMessage());
         }
